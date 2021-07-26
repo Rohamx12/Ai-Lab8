dc55608b4bd7df305304f91fc70cfe7ce0fdc1ed
Rohamx12 <roham.rl.roham@gmail.com>
2021-07-26 13:35:38 -0400

    starter

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -46,18 +46,27 @@ void StartScene::handleEvents()
 void StartScene::start()
 {
 	const SDL_Color blue = { 0, 0, 255, 255 };
+	const SDL_Color red = { 255, 0, 0, 255 };
+	const SDL_Color green = { 0, 255, 0, 255 };
 	m_pStartLabel = new Label("START SCENE", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	/*m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel->setParent(this);
+	addChild(m_pInstructionsLabel);*/
+
+	m_pInstructionsLabel = new Label("Roham Ali 101344253", "Consolas", 40, green, glm::vec2(400.0f, 100.0f));
 	m_pInstructionsLabel->setParent(this);
 	addChild(m_pInstructionsLabel);
 
+	m_pInstructionsLabel = new Label("Keneth Rodriguez", "Consolas", 40, red, glm::vec2(400.0f, 150.0f));
+	m_pInstructionsLabel->setParent(this);
+	addChild(m_pInstructionsLabel);
 
-	m_pShip = new Ship();
+	/*m_pShip = new Ship();
 	m_pShip->getTransform()->position = glm::vec2(400.0f, 300.0f); 
-	addChild(m_pShip); 
+	addChild(m_pShip); */
 
 	// Start Button
 	m_pStartButton = new Button();
